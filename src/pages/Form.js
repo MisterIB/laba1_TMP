d64895f18baecf7b25d46bd145bda0fa5675700b
MisterIB <163545760+MisterIB@users.noreply.github.com>
2025-04-09 14:17:16 +0700

    Create Form.js

---
+++ src/pages/Form.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import './Form.css'
+
+const Form = () => {
+
+    const [name, setName] = useState('');
+    const [manufacturer, setManufacturer] = useState('');
+    const navigate = useNavigate();
+
+    const handleNameChange = (e) => {
+        setName(e.target.value); 
+    };
+    const handleManufacturerChange = (e) => {
+        setManufacturer(e.target.value);
+    };
+   
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        axios.post('http://localhost:5000/items', { name, manufacturer })
+            .then(() => {
+                console.log("Data successfully sent to server");
+                navigate('/')
+            })
+            .catch((error) => {
+                console.error("Error sending data to server: ", error);
+            });
+    };
+
+    return (
+        <div className="form_page">
+            <form onSubmit={handleSubmit}>
+                <h1>Добавление детали</h1>
+                <label>
+                    Имя детали:
+                    <input type="text"
+                        value={name}
+                        onChange={handleNameChange}
+                        required
+                   />
+                </label>
+                <br/>
+        
+                <label>
+                    Статус:
+                    <input
+                        type="text"
+                        value={manufacturer}
+                        onChange={handleManufacturerChange}
+                        required
+                    />
+                </label>
+                <br/>
+
+                <button type="submit">Сохранить</button>
+            </form>
+        </div>
+    );  
+};
+export default Form;
