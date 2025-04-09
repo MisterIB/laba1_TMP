40583f5492f2f14542bf1d77c6a7f6c31a66c039
MisterIB <163545760+MisterIB@users.noreply.github.com>
2025-04-09 14:16:57 +0700

    Create Detail.js

---
+++ src/pages/Detail.js
@@ -0,0 +1,133 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
+import './Detail.css'
+
+const Detail = () => {
+
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    const [name,setName] = useState('');
+    const [manufacturer, setManufacturer] = useState('');
+    const [status,setStatus] = useState('');
+    const [errors, setErrors] = useState('');
+    const [isEditing, setIsEditing] = useState(false);
+
+    useEffect(() => {
+        const fetchDeviceData = async () => {
+            try {
+                const response = await axios.get(`http://localhost:5000/items/${id}`);
+                const { name, manufacturer, status, errors } = response.data;
+                setName(name);
+                setManufacturer(manufacturer);
+                setStatus(status);
+                setErrors(errors);
+            } catch (error) {
+                console.error("Ошибка при загрузке информации о детали: ", error);
+            }
+        };
+
+        fetchDeviceData();
+    }, [id]);
+
+    const handleDelete = async () => {
+        if (window.confirm("Вы уверены, что хотите удалить?")) {
+            try {
+                await axios.delete(`http://localhost:5000/items/${id}`);
+                navigate("/");
+
+            } catch (error) {
+                console.error("Ошибка при удалении компонента: ", error);
+            }
+        }
+    };
+
+    const handleSave = async () => {
+        try {
+            await axios.put(`http://localhost:5000/items/${id}`, { name, manufacturer, status, errors });
+            setIsEditing(false);
+        } catch (error) {
+            console.error("Ошибка при обновлении информации о детали: ", error);
+        }
+    }
+
+    return (
+        <div className="detail_page">
+          <h1>Информация о детали автомобиля</h1>
+          <div className="detail_info">
+            <label>
+              Название:{" "}
+              <strong>{name}</strong>
+            </label>
+            <br />
+            <label>
+              Производитель:{" "}
+              <strong>{manufacturer}</strong>
+            </label>
+            <br />
+            <label>
+              Статус:{" "}
+              <strong>{status}</strong>
+            </label>
+            <br />
+            <label>
+              Ошибки:{" "}
+              <strong>{errors}</strong>
+            </label>
+          </div>
+    
+          <div className="buttons_group">
+            <button onClick={() => navigate("/")}>Назад</button>
+            <button onClick={handleDelete}>
+              Удалить компонент
+            </button>
+            <button onClick={() => setIsEditing(true)}>
+              Изменить
+            </button>
+          </div>
+    
+          {isEditing && (
+            <div className="edit_form">
+              <h1>Редактирование информации</h1>
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  handleSave();
+                }}
+              >
+                <label>
+                  Новое имя:
+                  <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                  />
+                </label>
+                <br />
+                <label>
+                  Производитель:
+                  <input
+                    type="text"
+                    value={manufacturer}
+                    onChange={(e) => setManufacturer(e.target.value)}
+                    required
+                  />
+                </label>
+                <br />
+                <button type="submit">Сохранить</button>
+                <button
+                  type="button"
+                  onClick={() => setIsEditing(false)}
+                >
+                  Отмена
+                </button>
+              </form>
+            </div>
+          )}
+        </div>
+      );
+};
+
+export default Detail;
